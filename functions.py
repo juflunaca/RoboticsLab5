3cf5e449f8c1f0b19165a01863bb3ad1a8e6695e
Julian Luna <juflunaca@unal.edu.co>
2022-11-01 17:39:27 -0500

    Functions already working. Scripts in testing

--- functions.py
+++ functions.py
@@ -39,11 +39,13 @@ def invKinPhantomX(T):
         q[1] = q[1] - math.pi/2   
 
         #Solucion para q4
-        Rp = rotz(q[0]).transpose() @ np.array(T[0:3, 0:3])
-        pitch = math.atan2(float(Rp[2,0]),float(Rp[0,0]))
-        q[3] = float(pitch) - q[1] - q[2]
-        while q[3] > (7/6)*math.pi:
-            q[3] -= 2*math.pi
+        phi = math.atan2(T[2,2], math.sqrt(T[0,2]**2 +T[1,2]**2)) - math.pi/2
+        q[3] = phi - q[1] - q[2]
+        # Rp = rotz(q[0]).transpose() @ np.array(T[0:3, 0:3])
+        # pitch = math.atan2(float(Rp[2,0]),float(Rp[0,0]))
+        # q[3] = float(pitch) - q[1] - q[2]
+        # while q[3] > (7/6)*math.pi:
+        #     q[3] -= 2*math.pi
         q = [value*180/math.pi for value in q]
         return q
         
@@ -52,28 +54,30 @@ def invKinPhantomX(T):
 
 def drawGeometry(points:list, lineSteps:int=10):
     #Ubicar en posicion cercana al tablero para dibujar.
-    home = (0,0,33)
-    home2 = (0,5,10) #Calibrar altura!!!
+    pitchDraw = math.pi/2
+    home = (0,0,44.9,0)
+    home2 = (10,10,15,pitchDraw) #Calibrar altura!!!
     steps = stepCoordinates(home,home2,lineSteps)
-    for x,y,z in zip(*steps):
-        linearMove(x,y,z)
+    for x,y,z,pitch in zip(*steps):
+        print(x,y,z,pitch)
+        linearMove(x,y,z,pitch)
 
-    zDraw = 2  #Calibrar altura!!!
+    zDraw = 11.5  #Calibrar altura!!!
     previous=home2
     points.append(points[0]) # Para cerrar la geometria.
     for point in points:
-        steps = stepCoordinates(previous,(point[0],point[1],zDraw),lineSteps)
-        for x,y,z in zip(*steps):
-            linearMove(x,y,z)
-        previous = (point[0],point[1],zDraw)
+        steps = stepCoordinates(previous,(point[0],point[1],zDraw,pitchDraw),lineSteps)
+        for x,y,z,pitch in zip(*steps):
+            linearMove(x,y,z,pitch)
+        previous = (point[0],point[1],zDraw, pitchDraw)
     
     #Retornar a posicion cercana a dibujar y luego a home.
     steps = stepCoordinates(previous,home2,lineSteps)
-    for x,y,z in zip(*steps):
-        linearMove(x,y,z)
+    for x,y,z,pitch in zip(*steps):
+        linearMove(x,y,z,pitch)
     steps = stepCoordinates(home2,home,lineSteps)
-    for x,y,z in zip(*steps):
-        linearMove(x,y,z)
+    for x,y,z,pitch in zip(*steps):
+        linearMove(x,y,z,pitch)
         
 def jointCommand(id_num:int, addr_name:str, value:int, time):
     command = ""
@@ -105,37 +109,40 @@ def deg2TenBit(*deg, minValue:int=-150, maxValue:int=150):
 def stepCoordinates(origin:tuple, endpoint:tuple, steps:int):
     """Retorna una tupla con listas de coordenadas XYZ espaciadas un numero de steps
     desde un origen hasta un destino."""
-    if len(origin) != 3 or len(endpoint) != 3:
-        raise Exception("Los argumentos deben ser tuplas de puntos tridimensionales.")
-    xStep = endpoint[0] - origin[0]/steps
-    yStep = endpoint[1] - origin[1]/steps
-    zStep = endpoint[2] - origin[2]/steps
+    if len(origin) != 4 or len(endpoint) != 4:
+        raise Exception("Los argumentos deben ser tuplas de puntos tridimensionales y pitch.")
+    xStep = (endpoint[0] - origin[0])/steps
+    yStep = (endpoint[1] - origin[1])/steps
+    zStep = (endpoint[2] - origin[2])/steps
+    pitchStep = (endpoint[3] - origin[3])/steps
     x = []
     y = []
     z = []
+    pitch = []
     for i in range(1,steps+1):
         x.append(origin[0]+xStep*i)
         y.append(origin[1]+yStep*i)
         z.append(origin[2]+zStep*i)
-    return (x,y,z)
+        pitch.append(origin[3]+pitchStep*i)
+    return (x,y,z,pitch)
 
-def linearMove(x,y,z):
-    T = np.array(SE3.Trans(x,y,z))
-    print(T)
+def linearMove(x,y,z,pitch):
+    T = np.array(SE3.Trans(x,y,z)*SE3.Ry(pitch))
+    #print(T)
     q = invKinPhantomX(T)
     print(q)
     for value in q:
         if not -150 < value < 150:
             raise Exception("The value, or one of them, is outside the range of conversion.")
     for i, value in enumerate(q):
-        jointCommand(i+1,'Goal_Position',deg2TenBit(value),0.02)
+        jointCommand(i+1,'Goal_Position',deg2TenBit(value),0.001)
 
 def sendHome():
     q = [0,0,0,0]
     for i, value in enumerate(q):
         jointCommand(i+1,'Goal_Position',deg2TenBit(value),0.02)
 
-def limitTorque(limits:list=[500,400,300,300,300]):
+def limitTorque(limits:list=[500,400,300,300,600]):
     jointCommand(1, 'Torque_Limit', limits[0], 0)
     jointCommand(2, 'Torque_Limit', limits[1], 0)
     jointCommand(3, 'Torque_Limit', limits[2], 0)
@@ -144,5 +151,6 @@ def limitTorque(limits:list=[500,400,300,300,300]):
 
 def grip(value=850):
     if not 0 < value < 1023:
-        raise Exception("The value is outside the range.")
-    jointCommand(5, 'Goal_Position', value, 0)
+        print("The value is outside the range (0 to 1023).")
+    else:
+        jointCommand(5, 'Goal_Position', value, 0)
